4531224f828bc66f5df9bd3b6670aa10488df4e2
vchopin <804862438@qq.com>
2019-10-12 21:43:36 +0800

    DLL加测试

---
+++ wordCount/Core.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace wordCount
+{
+    public class Core:ICore
+    {
+        string content;
+        int lineCount = 0;
+        int charactersCount = 0;
+
+        public Core(string content)
+        {
+            this.content = content;
+        }
+
+        List<string> lists = new List<string>();
+
+        public int GetCharNum()
+        {
+            using (StringReader sr = new StringReader(content))
+            {
+                string line;
+                int lines = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lines++;
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        RegexAndAdd(line);
+                    }
+                }
+
+                charactersCount += (lines-1);
+            }
+
+            return charactersCount;
+        }
+
+        private void RegexAndAdd(string line)
+        {
+            lineCount++;
+            string regexStr1 = Regex.Replace(line, @"[^\u0000-\u007F]+", string.Empty);
+            charactersCount += regexStr1.Length;//统计每行的字符数 最后只需再加上每行就是总字符数
+
+
+            string regexStr2 = Regex.Replace(line, @"[^a-zA-Z0-9]+", "#");//过滤
+            string[] wordsArr1 = regexStr2.Split('#');
+            
+
+            foreach (string newWord in wordsArr1)
+            {
+                if (newWord.Length != 0)
+                {
+                    char[] temparr = newWord.ToCharArray();
+                    if ((newWord.Length >= 4) && (char.IsLetter(temparr[0]) && char.IsLetter(temparr[1]) && char.IsLetter(temparr[2]) && char.IsLetter(temparr[3])))
+                    {
+                        lists.Add(newWord.ToLower());
+                    }
+                }
+            }
+        }
+
+
+        public Dictionary<string, int> SortAndGetWord(Dictionary<string, int> wordsCount)
+        {
+            Dictionary<string, int> sortedWord = wordsCount.OrderByDescending(p => p.Value).ThenBy(p => p.Key).ToDictionary(p => p.Key, o => o.Value);
+            return sortedWord;
+        }
+
+        public int GetWordNum(Dictionary<string, int> wordsCount)
+        {
+            foreach (string li in lists)
+            {
+                if (wordsCount.ContainsKey(li))
+                {
+                    wordsCount[li]++;
+                }
+                else
+                {
+                    wordsCount.Add(li, 1);
+                }
+
+            }
+            return wordsCount.Count;//获得全部单词
+        }
+    }
+}
--- wordCount/Program.cs
+++ wordCount/Program.cs
@@ -3,90 +3,21 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace wordCount
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            string content = File.ReadAllText(args[0]);
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            ICore core = new Core(content);
+            core.GetCharNum();
+            Console.WriteLine(core.GetWordNum(words));
+            new DataIO().Print(core.SortAndGetWord(words), 10);
 
-            int charactersCount = 0;
-            int lineCount = 0;
-            int wordCount = 0;
-            int lines = 0;
-            string path = args[0];
-            List<string> lists = new List<string>();  //存储过滤后单词的列表  
-
-            if (File.Exists(path))
-            {
-                StreamReader sr = new StreamReader(path, Encoding.UTF8);
-                
-                string readLine="";
-                while ((readLine = sr.ReadLine()) != null)
-                {
-                    lines++;
-                    if (!string.IsNullOrWhiteSpace(readLine))
-                    {
-                        lineCount++;
-                        string regexStr = Regex.Replace(readLine, @"[^a-zA-Z0-9]+", "#");//过滤
-                        string[] wordsArr1 = regexStr.Split('#');  
-                        charactersCount += readLine.Length;//统计每行的字符数 最后只需再加上每行就是总字符数
-
-                        foreach (string newWord in wordsArr1)
-                        {
-                            if (newWord.Length != 0)
-                            {
-                                char[] temparr = newWord.ToCharArray();
-                                if ((newWord.Length >= 4) && (char.IsLetter(temparr[0]) && char.IsLetter(temparr[1]) && char.IsLetter(temparr[2]) && char.IsLetter(temparr[3])))
-                                {
-                                    lists.Add(newWord.ToLower());
-                                }
-                            }
-                        }  
-                    }
-                    
-                }
-                charactersCount += lines;
-                sr.Close();
-            }
-
-
-            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
-
-            //单词出现频率统计
-            foreach (string li in lists)
-            {
-                if (wordsCount.ContainsKey(li))
-                {
-                    wordsCount[li] ++;
-                }
-                else
-                {
-                    wordsCount.Add(li, 1);
-                }
-
-            }
-
-            wordCount = wordsCount.Count;//获得全部单词
-
-            Console.WriteLine("characters: " + charactersCount);
-            Console.WriteLine("words: " + wordCount);
-            Console.WriteLine("lines: " + lineCount);
-
-            //开始排序 先按照频率排序，在按照字典序排序
-            Dictionary<string, int> sortedWord = wordsCount.OrderByDescending(p => p.Value).ThenBy(p => p.Key).ToDictionary(p => p.Key, o => o.Value);
-            int i = 0;
-            foreach (KeyValuePair<string, int> item in sortedWord)
-            {
-                
-                Console.WriteLine(item.Key+" "+item.Value);
-                if (i == 9)
-                    break;
-                i++;
-            }
-            Console.ReadLine();
         }
     }
 }
